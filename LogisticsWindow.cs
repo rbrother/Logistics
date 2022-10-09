a0408264516b2f8fec2b4c4055935183c42707c1
Robert Brotherus <robert@iki.fi>
2022-10-09 09:27:02 +0300

    Initial version

---
+++ LogisticsWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Media;
+using System.Numerics;
+using System.Timers;
+using System.Diagnostics;
+
+public class LogisticsWindow : PlotWindow {
+
+    double r_min = 3.5;
+    double r_max = 4.0;
+    double step = 0.0000001;
+
+    protected override void DoPlot( ) {
+        var start = DateTime.Now;
+        double x = 0.1;
+        double[,] plotLevel = new double[PixelWidth,PixelHeight];
+        for (double r = r_min; r < r_max; r += step)
+        {
+            x = x * r * (1 - x);
+            double relativeScreenX = (r - r_min) / (r_max - r_min);
+            int screenX = Math.Min( Convert.ToInt32(relativeScreenX * PixelWidth), PixelWidth-1);
+            int screenY = Math.Min( Convert.ToInt32( PixelHeight * (1-x)), PixelHeight-1);
+            plotLevel[screenX, screenY] = Math.Min(plotLevel[screenX, screenY] + 0.1, 1.0);
+            var level = Convert.ToByte((1.0-plotLevel[screenX, screenY]) * 255.0);
+            DrawPixel(screenX, screenY, Color.FromRgb(level, level, level));
+        }
+        Debug.WriteLine($"Duration: {DateTime.Now - start}");
+    }
+
+} // class
---
+++ PlotWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+using System.Numerics;
+
+abstract public class PlotWindow : Window {
+    Image image;
+    WriteableBitmap Bmp;
+    
+    public int PixelWidth { get { return Convert.ToInt32( SystemParameters.PrimaryScreenWidth ); } }
+    public int PixelHeight { get { return Convert.ToInt32( SystemParameters.PrimaryScreenHeight ); } }
+
+    protected abstract void DoPlot();
+
+    public PlotWindow( ) {
+        this.WindowState = System.Windows.WindowState.Maximized;
+        this.WindowStyle = System.Windows.WindowStyle.None;
+        image = new Image( );
+        this.Content = image;
+        image.MouseLeftButtonUp += image_MouseUp;
+        Bmp = new WriteableBitmap( PixelWidth, PixelHeight, 96, 96, PixelFormats.Bgra32, null );
+        image.Source = Bmp;
+        DoPlot();
+    }
+
+    void image_MouseUp( object sender, System.Windows.Input.MouseButtonEventArgs e ) {
+        var pos = e.MouseDevice.GetPosition( image );
+        // Center = Origin + new Complex( pos.X, pos.Y ) * PixelStep;
+    }
+
+    protected void DrawPixel( int x, int y, Color color ) {
+        byte[] buffer = new byte[4];
+        buffer[0] = color.B;
+        buffer[1] = color.G;
+        buffer[2] = color.R;
+        buffer[3] = color.A;
+        Bmp.WritePixels(new Int32Rect(0, 0, 1, 1), buffer, 4, 
+            Math.Min( x, PixelWidth-1), Math.Min(y, PixelHeight-1));
+    }
+
+}
