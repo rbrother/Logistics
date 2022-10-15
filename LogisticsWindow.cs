ad8f76abb99543f7d13d251d207f6855ad60bd7e
Robert Brotherus <robert@iki.fi>
2022-10-15 19:59:50 +0300

    Dynamic number of iterations per column to get certain color level

--- LogisticsWindow.cs
+++ LogisticsWindow.cs
@@ -6,28 +6,43 @@ using System.Diagnostics;
 
 public class LogisticsWindow : PlotWindow
 {
-    readonly double r_min = 3.5, r_max = 4.0;
+    readonly double r_min = 3.4, r_max = 4.0;
     readonly double x_min = 0.0, x_max = 1.0;
-    readonly double step = 0.00000003;
+    double[,] plotLevel;
 
     double Intensify(double level) => level + (1.0 - level) * 0.2;
 
     protected override void DoPlot()
     {
+        int totalIterations = 0;
         double x = 0.1;
-        double[,] plotLevel = new double[PixelWidth+1, PixelHeight+1];
-        for (double r = r_min; r < r_max; r += step)
+        double r = r_min;
+        double column_r_step = (r_max - r_min) / PixelWidth;
+        plotLevel = new double[PixelWidth+1, PixelHeight+1];
+        for (int screenColumn = 0; screenColumn < PixelWidth; screenColumn++)
         {
-            x = x * r * (1 - x);
-            var relativeScreenX = (r - r_min) / (r_max - r_min);
-            var relativeScreenY = (x - x_min) / (x_max - x_min);
-            var screenX = Convert.ToInt32(relativeScreenX * PixelWidth);
-            var screenY = Convert.ToInt32(PixelHeight * (1 - relativeScreenY));
-            if (screenX >= 0 && screenY >= 0 && screenX < PixelWidth && screenY < PixelHeight) { 
-                plotLevel[screenX, screenY] = Intensify(plotLevel[screenX, screenY]);
+            var colFinished = false;
+            while(!colFinished) {
+                totalIterations++;
+                x = x * r * (1 - x);
+                colFinished = AddPoint(screenColumn, x);
             }
+            r += column_r_step;
         }
         DrawPixels(plotLevel);
+        Console.WriteLine($"totalIterations: {totalIterations}");
     }
 
+    private bool AddPoint(int screenColumn, double x)
+    {
+        var relativeScreenY = (x - x_min) / (x_max - x_min);
+        var screenY = Convert.ToInt32(PixelHeight * (1 - relativeScreenY));
+        if (screenY >= 0 && screenY < PixelHeight)
+        {
+            var newLevel = Intensify(plotLevel[screenColumn, screenY]);
+            plotLevel[screenColumn, screenY] = newLevel;
+            if (newLevel > 0.99) return true;
+        }
+        return false;
+    }
 } // class
